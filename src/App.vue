962664fcc7ad9fdd0787cf48fd8830876faf8f50
Jacob <logasja@outlook.com>
2020-08-18 13:26:53 -0400

    update the app component so it actually uses the router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <router-view> </router-view>
   </div>
 </template>
 
 <script lang="ts">
 import "@/assets/styles.css";
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 });
 </script>
 
